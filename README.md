b9bb410becf00ac6fbf960cc3b436118414927a0
Aluno SENAI <Aluno@SN-368554.edu.fiesc.com.br>
2024-05-16 11:23:32 -0300

    jogo base

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Jogo_advinha-java
 jogo advinhação desenvolvido na UC de Logica de programação do projeto Hellow Blumenau.
+
+
+Desenvolvido fase inicial do aplicativo e lógica por tras da tomada de decisõess do programa.
+
+Utilizando o comando Switch para verificar as decisões de jogo do usuário, bem como o comando IF para validar se o valor digitado pelo usuário é igual ao valor "pensando"
+
+# Proximas features 
+
+Geração de resposta aleatória
+Poder jogar mais de uma vez
+stema de pontuação
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,48 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner leitura = new Scanner(System.in);
+        String pronto;
+        int resposta;
+        int tentativa;
+
+
+        System.out.println("  _____________________________________________________");
+        System.out.println("               ⭐ JOGO DE ADVINHAÇÃO  ⭐                ");
+        System.out.println("  _____________________________________________________");
+        System.out.println();
+        System.out.println("❗ REGRAS DO JOGO ❗  ");
+        System.out.println();
+        System.out.println("1- Vou pensar em um número ");
+        System.out.println();
+        System.out.println("2- Você tenta advinhar qual número estou pensando ");
+        System.out.println();
+        System.out.println("3- Te informo se se acertou ou não ");
+        System.out.println();
+        System.out.println("❓ Está pronto para jogar (s/n) ❓");
+        pronto = leitura.next();
+
+        switch (pronto){
+            case "s":
+                resposta =8;
+                System.out.println(" Qual número entre 0 e 10 estou pensando ❓");
+                tentativa = leitura.nextInt();
+                if(tentativa == resposta){
+                    System.out.println("✔️ Parabéns, certa resposta! ✔️");
+                }else {
+                    System.out.println("❌ Que pena, você errou! ❌");
+                }
+                break;
+            case "n":
+                System.out.println(";( Que pena, até mais ;(");
+                break;
+            default:
+                System.out.println("⚠️ OPÇÃO INVÁLIDA! ⚠️");
+        }
     }
+
 }
